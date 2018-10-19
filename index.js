392f49acf6446f8bb423c6b15f7bb35e7971ff0f
E.O. Stinson <yostinso@gmail.com>
2018-10-19 15:38:40 -0700

    Initial working version.
    TODO: prompt on reload/window close not just quit.

--- index.js
+++ index.js
@@ -1,27 +1,50 @@
 const { dialog } = require('electron');
+const psList = require('ps-list');
 
+let enabled;
+let ignored;
 let quitHandler;
+let setPidsHandler;
+let runningPids = {};
 
 const createQuitHandler = (app) => {
   let quitting = false;
   return (evt) => {
-    if (enabled && !quitting && app.getWindows().size) {
+    if (enabled && !quitting && app.getWindows().size && Object.keys(runningPids).length > 0) {
       evt.preventDefault();
-      dialog.showMessageBox({
-        type: 'question',
-        buttons: ['OK', 'Cancel'],
-        defaultId: 0,
-        title: 'Quit Hyper?',
-        message: 'Quit Hyper?',
-        detail: 'All windows and active applications will be closed.'
-      }, (selectedButtonIdx) => {
-        switch (selectedButtonIdx) {
-          case 0: // OK
-            quitting = true;
-            app.quit();
-            break;
-          default:
-            break;
+
+      const pids = Object.values(runningPids).reduce((memo, windowPids) => {
+        return memo.concat(windowPids);
+      }, []);
+      psList().then((procs) => {
+        const children = procs.filter((proc) => {
+          return pids.some((pid) => pid === proc.ppid)
+        }).filter((child) => {
+          return !ignored.some((cmd) => { return cmd.test(child.cmd); });
+        });
+
+        if (children.length > 0) {
+          const runningNames = children.map((c) => c.cmd);
+          dialog.showMessageBox({
+            type: 'question',
+            buttons: ['OK', 'Cancel'],
+            defaultId: 0,
+            title: 'Quit Hyper?',
+            message: 'Quit Hyper and ' + children.length + ' running processes?',
+            detail: 'The following processes are active:\n' + runningNames.map((rn) => "  " + rn).join("\n")
+          }, (selectedButtonIdx) => {
+            switch (selectedButtonIdx) {
+              case 0: // OK
+                quitting = true;
+                app.quit();
+                break;
+              default:
+                break;
+            }
+          });
+        } else {
+          quitting = true;
+          app.quit();
         }
       });
     }
@@ -30,7 +53,28 @@ const createQuitHandler = (app) => {
 
 exports.decorateConfig = (config) => {
   enabled = true;
+  ignored = [ /^\/bin\/bash/ ];
   return config;
+};
+
+exports.mapTermsState = (props, state) => {
+  const sessions = Object.values(props.sessions.sessions);
+  const pids = sessions.map((s) => s.pid);
+  if (window.rpc.id) {
+    window.rpc.emit("set term pids", { pids });
+  }
+  return state;
+};
+
+exports.onWindow = (win) => {
+  win.rpc.on('set term pids', ({ pids }) => {
+    console.log("Setting PIDs for " + win.id + " to ", pids);
+    runningPids[win.id] = pids;
+  });
+  win.on('close', () => {
+    console.log("Clearing PIDs for " + win.id);
+    delete runningPids[win.id];
+  });
 };
 
 exports.onApp = (app) => {
@@ -39,9 +83,9 @@ exports.onApp = (app) => {
   app.on('before-quit', quitHandler);
 };
 
-export.onUnload = (app) => {
+exports.onUnload = (app) => {
   if (quitHandler) {
     app.off('before-quit', quitHandler);
     quitHandler = null;
-  }
+  } // Plugin was reloaded
 };

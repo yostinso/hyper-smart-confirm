576c6b10794f044079fae7c042924a2539810e53
E.O. Stinson <yostinso@gmail.com>
2018-10-24 17:19:35 -0700

    Initial useful commit
    This seems to work with window close, app quit, and tab/termgroup quit.

--- index.js
+++ index.js
@@ -1,66 +1,92 @@
-const { dialog } = require('electron');
+const { app, dialog } = require('electron');
+const remote = require('electron').remote;
 const psList = require('ps-list');
 
-let enabled;
-let enabledForWindows;
-let ignored;
+let quitting = false;
+let decoratedConfig;
 let quitHandler;
 let setPidsHandler;
 let runningPids = {};
+let debugEnabled = null;
 
-const promptToQuit = (windowId) => {
+const getConfig = () => decoratedConfig ? decoratedConfig : window.config.getConfig();
+const pluginConfigSelector = () => getConfig().hyperSmartConfirm || {};
+const enabledSelector = () => pluginConfigSelector().enabled;
+const ignoredSelector = () => pluginConfigSelector().ignored;
+const ignoreShellSelector = () => pluginConfigSelector().ignoreShell;
+const enabledForWindowsSelector = () => pluginConfigSelector().enabledForWindows;
+const debugEnabledSelector = () => pluginConfigSelector().debugEnabled;
+
+const debugLog = () => {
+  if (debugEnabled === null) { debugEnabled = !!debugEnabledSelector(); }
+  if (debugEnabled) {
+    console.log.apply(console, arguments);
+  }
+}
+
+const askConfirmQuit = (actionName, message, detail) => {
   return new Promise((resolve) => {
-    let pids;
-    if (!windowId) {
-      pids = Object.values(runningPids).reduce((memo, windowPids) => {
-        return memo.concat(windowPids);
-      }, []);
-    } else {
-      pids = runningPids[windowId];
-    }
-    console.log(runningPids);
-    console.log('Window ' + (windowId || '[all]') + ' has pids', pids);
-
-    const actionName = windowId ? "Quit Hyper" : "Close window";
-    psList().then((procs) => {
-      const children = procs.filter((proc) => {
-        return pids.some((pid) => pid === proc.ppid)
-      }).filter((child) => {
-        return !ignored.some((cmd) => { return cmd.test(child.cmd); });
-      });
-
-      if (children.length > 0) {
-        const runningNames = children.map((c) => c.cmd);
-        dialog.showMessageBox({
-          type: 'question',
-          buttons: ['OK', 'Cancel'],
-          defaultId: 0,
-          title: actionName + '?',
-          message: actionName + ' and ' + children.length + ' running processes?',
-          detail: 'The following processes are active:\n' + runningNames.map((rn) => "  " + rn).join("\n")
-        }, (selectedButtonIdx) => {
-          switch (selectedButtonIdx) {
-            case 0: // OK
-              resolve(true);
-              break;
-            default:
-              resolve(false);
-              break;
-          }
-        });
-      } else {
-        resolve(true);
-      }
+    const d = remote ? remote.dialog : dialog;
+    d.showMessageBox({
+      type: 'question',
+      buttons: ['OK', 'Cancel'],
+      defaultId: 0,
+      title: actionName + '?',
+      message: message,
+      detail: detail
+    }, (selectedButtonIdx) => {
+      resolve(selectedButtonIdx === 0); // OK button is 0
     });
   });
 }
 
+const promptToQuit = (ptys, actionName) => {
+  return new Promise((resolve) => {
+    const ignored = ignoredSelector();
+    psList().then((procs) => {
+      const ignoreShell = ignoreShellSelector();
+      const children = procs.filter((proc) => {
+        return ptys.some(({ pid }) => (pid === proc.ppid) || (pid === proc.pid))
+      }).filter((child) => {
+        const pty = ptys.find(({ pid }) => (pid === child.ppid) || (pid === child.pid));
+        const isIgnored = ignored.some((cmd) => { return cmd.test(child.cmd); });
+        if (!isIgnored && ignoreShell && pty) {
+          const childBin = child.cmd.split(' ')[0].match(/[^\/]*$/)[0];
+          const ptyBin = (pty.shell || pty._file || "").split(' ')[0].match(/[^\/]*$/)[0];
+          const isShell = childBin === ptyBin && childBin !== "";
+          return !isShell;
+        } else {
+          return true;
+        }
+      });
+
+
+      if (children.length > 0) {
+        const runningNames = children.map((c) => c.cmd);
+        askConfirmQuit(
+          actionName, 
+          actionName + ' and ' + children.length + ' running processes?',
+          'The following processes are active:\n' + runningNames.map((rn) => "  " + rn).join("\n")
+        ).then((result) => resolve(result));
+      } else {
+        console.log("Quit now; no children");
+        resolve(true);
+      }
+    });
+  });
+};
+
 const createQuitHandler = (app) => {
-  let quitting = false;
   return (evt) => {
-    if (enabled && !quitting && app.getWindows().size && Object.keys(runningPids).length > 0) {
+    const enabled = enabledSelector();
+    if (enabled && !quitting && app.getWindows().size) {
       evt.preventDefault();
-      promptToQuit().then((shouldQuit) => {
+      const ptys = Array.from(app.getWindows()).map(({ sessions }) => {
+        return Array.from(sessions.values()).map(({ pty }) => pty);
+      }).reduce((memo, ptys) => memo.concat(ptys), []);
+      console.log("App closed with pids ", ptys.map((pty) => pty.pid));
+      const ignored = ignoredSelector();
+      promptToQuit(pty, "Quit Hyper", ignored).then((shouldQuit) => {
         if (shouldQuit) {
           quitting = true;
           app.quit();
@@ -68,16 +94,18 @@ const createQuitHandler = (app) => {
       });
     }
   };
-}
+};
 
 const createCloseWindowHandler = (win) => {
-  // Hyper doesn't handle this well so
-  // we should probably intercept SESSION_USER_EXIT
   let closing = false;
   return (evt) => {
-    if (enabledForWindows && !closing) {
+    const enabledForWindows = enabledForWindowsSelector();
+    const ignored = ignoredSelector();
+    if (enabledForWindows && !closing && !quitting) {
       evt.preventDefault();
-      promptToQuit(win.id).then((shouldQuit) => {
+      const ptys = Array.from(win.sessions.values()).map(({ pty }) => pty);
+      console.log("Window ", win.getTitle(), ":", win.id, " closed with pids ", ptys.map((pty) => pty.pid));
+      promptToQuit(ptys, "Close window", ignored).then((shouldQuit) => {
         if (shouldQuit) {
           closing = true;
           win.close();
@@ -88,38 +116,53 @@ const createCloseWindowHandler = (win) => {
 };
 
 exports.decorateConfig = (config) => {
-  enabled = true;
-  enabledForWindows = true;
-  ignored = []; //[ /^\/bin\/bash/ ];
-  return config;
+  const hyperSmartConfirm = config.hyperSmartConfirm || {};
+  const newPluginConfig = Object.assign({}, 
+    hyperSmartConfirm,
+    {
+      enabled: hyperSmartConfirm.enabled || true,
+      enabledForWindows: hyperSmartConfirm.enabled || true,
+      ignored: hyperSmartConfirm.ignored || [],
+      ignoreShell: hyperSmartConfirm.ignoreShell || true
+    }
+  );
+  decoratedConfig = Object.assign({}, config, { hyperSmartConfirm: newPluginConfig });
+  return decoratedConfig;
 };
 
-exports.mapTermsState = (props, state) => {
-  const sessions = Object.values(props.sessions.sessions);
-  const pids = sessions.map((s) => s.pid);
-  if (window.rpc.id) {
-    if (pids.length === 0) { debugger; }
-    console.log("Term state sessions:", sessions);
-    window.rpc.emit("set term pids", { pids });
+exports.middleware = (store) => (next) => (action) => {
+  const ignored = ignoredSelector();
+  const enabledForWindows = enabledForWindowsSelector();
+
+  if (action.type === 'TERM_GROUP_EXIT' && enabledForWindows) {
+    const {sessionUid} = store.getState().termGroups.termGroups[action.uid];
+    const {shell, pid} = store.getState().sessions.sessions[sessionUid];
+    console.log("Session ", sessionUid, " closed with pid ", pid);
+    promptToQuit([{ shell, pid }], "Close session").then((shouldQuit) => {
+      if (shouldQuit) {
+        console.log("Quitting...");
+        next(action);
+      } else {
+        console.log("Quit cancelled...");
+      }
+    });
+  } else {
+    next(action);
   }
-  return state;
 };
 
 exports.onWindow = (win) => {
-  const windowId = win.id;
-  win.rpc.on('set term pids', ({ pids }) => {
-    console.log("Setting PIDs for " + windowId + " to ", pids);
-    runningPids[win.id] = pids;
-  });
   win.on('close', createCloseWindowHandler(win));
-  win.on('closed', () => {
-    console.log("Clearing PIDs for " + windowId);
-    delete runningPids[windowId];
-  });
+  const oldOn = win.on;
+  win.on = (evtType, handler, ...extraArgs) => {
+    const wrappedHandler = (evt, ...evtArgs) => {
+      if (!evt.defaultPrevented) { handler(evt, ...evtArgs); }
+    };
+    return oldOn.call(win, evtType, wrappedHandler, ...extraArgs);
+  };
 };
 
 exports.onApp = (app) => {
-  if (quitHandler) { app.off('before-quit', quitHandler); } // Plugin was reloaded
   quitHandler = createQuitHandler(app);
   app.on('before-quit', quitHandler);
 };
